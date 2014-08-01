f5b41ca3b23965af704f048f0055ad430148e623
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2014-08-01 19:05:21 +0900

    ポート番号を環境変数から取ります。

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var connect = require('connect'),
   serveStatic = require('serve-static'),
   http = connect()
   .use(serveStatic(__dirname))
-  .listen(3000),
+  .listen(process.env.PORT || 3000),
   mows = require('mows'),
   mows_broadcast = require('./mows_broadcast'),
   server = mows.attachServer(http, mows_broadcast);
